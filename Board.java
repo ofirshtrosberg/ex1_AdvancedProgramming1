01645981572fd9ccae104c3a02f7508e9c68c71e
ofirshtrosberg <ofiri112@gmail.com>
2022-12-03 03:58:38 +0200

    3:58

--- Board.java
+++ Board.java
@@ -3,61 +3,53 @@ package test;
 import java.util.Arrays;
 
 public class Board {
+    private int turn;
     private static int[][] wordScore = null;
     private static int[][] letterScore = null;
     private Tile[][] boardTiles;
     private static Board board = null;
-    private void createScores(){
+
+    private void createScores() {
         wordScore = new int[15][15];
         letterScore = new int[15][15];
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
-                if(i==j) {
-                    if (i == 0||i==14)
+                if (i == j) {
+                    if (i == 0 || i == 14)
                         wordScore[i][j] = 3;
-                    else if((i>=1&&i<=4)||(i>=10&&i<=13)||i==7)
+                    else if ((i >= 1 && i <= 4) || (i >= 10 && i <= 13) || i == 7)
                         wordScore[i][j] = 2;
-                    else if(i==5||i==9)
+                    else if (i == 5 || i == 9)
                         letterScore[i][j] = 3;
                     else
-                        letterScore[i][j] =2;
-                }
-                else if(i==14-j) {
-                    if (i == 0||i==14)
+                        letterScore[i][j] = 2;
+                } else if (i == 14 - j) {
+                    if (i == 0 || i == 14)
                         wordScore[i][j] = 3;
-                    else if((i>=1&&i<=4)||(i>=10&&i<=13))
+                    else if ((i >= 1 && i <= 4) || (i >= 10 && i <= 13))
                         wordScore[i][j] = 2;
-                    else if(i==5||i==9)
+                    else if (i == 5 || i == 9)
                         letterScore[i][j] = 3;
                     else
-                        letterScore[i][j] =2;
-                }
-                else{
-                    if(((i==0||i==14)&&j==7)||((j==0||j==14)&&i==7)){
+                        letterScore[i][j] = 2;
+                } else {
+                    if (((i == 0 || i == 14) && j == 7) || ((j == 0 || j == 14) && i == 7)) {
                         wordScore[i][j] = 3;
-                    }
-                    else if((i==0||i==14)&&(j==3||j==11)){
+                    } else if ((i == 0 || i == 14) && (j == 3 || j == 11)) {
                         letterScore[i][j] = 2;
-                    }
-                    else if((i==3||i==11)&&(j==0||j==14)){
+                    } else if ((i == 3 || i == 11) && (j == 0 || j == 14)) {
                         letterScore[i][j] = 2;
-                    }
-                    else if((i==1||i==13)&&(j==5||j==9)){
+                    } else if ((i == 1 || i == 13) && (j == 5 || j == 9)) {
                         letterScore[i][j] = 3;
-                    }
-                    else if((j==1||j==13)&&(i==5||i==9)){
+                    } else if ((j == 1 || j == 13) && (i == 5 || i == 9)) {
                         letterScore[i][j] = 3;
-                    }
-                    else if((i==6||i==8)&&(j==2||j==12)){
+                    } else if ((i == 6 || i == 8) && (j == 2 || j == 12)) {
                         letterScore[i][j] = 2;
-                    }
-                    else if((j==6||j==8)&&(i==2||i==12)){
+                    } else if ((j == 6 || j == 8) && (i == 2 || i == 12)) {
                         letterScore[i][j] = 2;
-                    }
-                    else if((i==3||i==11)&&j==7){
+                    } else if ((i == 3 || i == 11) && j == 7) {
                         letterScore[i][j] = 2;
-                    }
-                    else if((j==3||j==11)&&i==7) {
+                    } else if ((j == 3 || j == 11) && i == 7) {
                         letterScore[i][j] = 2;
                     }
                 }
@@ -65,26 +57,192 @@ public class Board {
         }
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
-                if(wordScore[i][j]==0)
-                    wordScore[i][j]=1;
-                if(letterScore[i][j]==0)
-                    letterScore[i][j]=1;
+                if (wordScore[i][j] == 0)
+                    wordScore[i][j] = 1;
+                if (letterScore[i][j] == 0)
+                    letterScore[i][j] = 1;
             }
         }
     }
-    private Board(){
-        if(letterScore==null&&wordScore==null)
+
+    private Board() {
+        if (letterScore == null && wordScore == null)
             createScores();
         boardTiles = new Tile[15][15];
+        turn = 1;
     }
-    public static Board getBoard()
-    {
+
+    public static Board getBoard() {
         if (board == null)
             board = new Board();
         return board;
     }
-    Tile[][] getTiles(){
+
+    Tile[][] getTiles() {
         ///// check if works
         return Arrays.stream(boardTiles).map(Tile[]::clone).toArray(Tile[][]::new);
+    }
+
+    boolean passTroughMiddleTile(Word word){
+        int row = word.getRow();
+        int col = word.getCol();
+        int length = word.getTiles().length;
+        if(word.isVertical()){
+            if(col!=7)
+                return false;
+            if(row<=7&&row+length>=7)
+                return true;
+        }
+        else{
+            if(row!=7)
+                return false;
+            if(col<=7&&col+length>=7)
+                return true;
+        }
+        return false;
+    }
+    boolean passTroughOrNearTile(Word word){
+        int row = word.getRow();
+        int col = word.getCol();
+        int length = word.getTiles().length;
+        for (int i = 0; i < length; i++) {
+            if (word.isVertical()) {
+                if (boardTiles[row + i][col] != null)
+                    return true;
+                if (col + 1 <= 14)
+                    if (boardTiles[row + i][col + 1] != null)
+                        return true;
+                if (col - 1 >= 0)
+                    if (boardTiles[row + i][col - 1] != null)
+                        return true;
+                if (row + 1 <= 14)
+                    if (boardTiles[row + i + 1][col] != null)
+                        return true;
+                if (row - 1 >= 0)
+                    if (boardTiles[row + i - 1][col] != null)
+                        return true;
+
+            } else {
+                if (boardTiles[row][col + i] != null)
+                    return true;
+                if (col + 1 <= 14)
+                    if (boardTiles[row][col + i + 1] != null)
+                        return true;
+                if (col - 1 >= 0)
+                    if (boardTiles[row][col + i - 1] != null)
+                        return true;
+                if (row + 1 <= 14)
+                    if (boardTiles[row + 1][col + i] != null)
+                        return true;
+                if (row - 1 >= 0)
+                    if (boardTiles[row - 1][col + i] != null)
+                        return true;
+            }
+        }
+        return false;
+    }
+    boolean requiresTilesChanges(Word word){
+        int row = word.getRow();
+        int col = word.getCol();
+        int length = word.getTiles().length;
+        for (int i = 0; i < length; i++) {
+            if(word.getTiles()[i]==null)
+                continue;
+            if(word.isVertical()){
+                if(boardTiles[row+i][col]==null)
+                    continue;
+                if(boardTiles[row+i][col]!=word.getTiles()[i])
+                    return true;
+            }
+            else{
+                if(boardTiles[row][col+i]==null)
+                    continue;
+                if(boardTiles[row][col+i]!=word.getTiles()[i])
+                    return true;
+            }
+        }
+        return false;
+    }
+    boolean boardLegal(Word word) {
+        if (word == null)
+            return false; //?!?!
+        int row = word.getRow();
+        int col = word.getCol();
+        int length = word.getTiles().length;
+        if (row > 14 || row < 0 || col > 14 || col < 0)
+            return false;
+        if (word.isVertical() && row + length > 14)
+            return false;
+        else if (!word.isVertical() && col + length > 14)
+            return false;
+        //turn 1 must use the middle tile
+        if (turn == 1) {
+            return passTroughMiddleTile(word);
+        }
+        return passTroughOrNearTile(word)&&!requiresTilesChanges(word);
+    }
+
+    boolean dictionaryLegal(Word word) {
+        return true;
+    }
+//    int scoreOfNewWordsCreated(){
+//
+//    }
+    int getScore(Word word) {
+        if (word == null || !boardLegal(word) || !dictionaryLegal(word))
+            return 0;
+        int wordLength = word.getTiles().length;
+        int lettersScore = 0;
+        int wordBonusesMultipleBy = 1;
+        for (int i = 0; i < wordLength; i++) {
+//            if(word.getTiles()[i]==null){
+//                continue;
+//            }
+            if (word.isVertical()) {
+                if(word.getTiles()[i]==null){
+                    lettersScore += (boardTiles[i + word.getRow()][word.getCol()].score) * (letterScore[i + word.getRow()][word.getCol()]);
+                    wordBonusesMultipleBy *= wordScore[i + word.getRow()][word.getCol()];
+                    continue;
+                }
+                lettersScore += (word.getTiles()[i].score) * (letterScore[i + word.getRow()][word.getCol()]);
+                wordBonusesMultipleBy *= wordScore[i + word.getRow()][word.getCol()];
+            } else {
+                if(word.getTiles()[i]==null){
+                    lettersScore += (boardTiles[word.getRow()][i + word.getCol()].score) * (letterScore[word.getRow()][i + word.getCol()]);
+                    wordBonusesMultipleBy *= wordScore[word.getRow()][i + word.getCol()];
+                    continue;
+                }
+                lettersScore += (word.getTiles()[i].score) * (letterScore[word.getRow()][i + word.getCol()]);
+                wordBonusesMultipleBy *= wordScore[word.getRow()][i + word.getCol()];
+            }
+        }
+        return lettersScore * wordBonusesMultipleBy;
+    }
+    int tryPlaceWord(Word word){
+        if(!dictionaryLegal(word)||!boardLegal(word)||word==null)
+            return 0;
+
+        int row = word.getRow();
+        int col = word.getCol();
+        int length = word.getTiles().length;
+        for (int i = 0; i < length; i++) {
+            if(word.isVertical()) {
+                if (word.getTiles()[i] == null)
+                    continue;
+                boardTiles[row + i][col] = word.getTiles()[i];
+            }
+            else
+            if (word.getTiles()[i] == null)
+                continue;
+                boardTiles[row][col+i] = word.getTiles()[i];
+
+        }
+        if(turn==1){
+            int score = getScore(word);
+            turn++;
+            wordScore[7][7]=1;
+            return score;
+        }
+        return getScore(word);
     }
 }
--- MainTrain.java
+++ MainTrain.java
@@ -54,8 +54,7 @@ public class MainTrain {
 		Board b = Board.getBoard();
 		if(b!=Board.getBoard())
 			System.out.println("board should be a Singleton (-5)");
-//		Tile[][] tiles = b.getTiles();
-
+		//Tile[][] tiles = b.getTiles();
 		Bag bag = Bag.getBag();
 		Tile[] ts=new Tile[10];
 		for(int i=0;i<ts.length;i++)
@@ -68,34 +67,34 @@ public class MainTrain {
 		Word w4=new Word(ts,7,-1,false);
 		Word w5=new Word(ts,0,7,true);
 		Word w6=new Word(ts,7,0,false);
-//
-//		if(b.boardLegal(w0) || b.boardLegal(w1) || b.boardLegal(w2) || b.boardLegal(w3) || b.boardLegal(w4) || !b.boardLegal(w5) || !b.boardLegal(w6))
-//			System.out.println("your boardLegal function is wrong (-10)");
-//
-//		for(Tile t : ts)
-//			bag.put(t);
-//
-//		Word horn=new Word(get("HORN"), 7, 5, false);
-//		if(b.tryPlaceWord(horn)!=14)
-//			System.out.println("problem in placeWord for 1st word (-10)");
-//
-//		Word farm=new Word(get("FA_M"), 5, 7, true);
-//		if(b.tryPlaceWord(farm)!=9)
-//			System.out.println("problem in placeWord for 2ed word (-10)");
-//
-//		Word paste=new Word(get("PASTE"), 9, 5, false);
-//		if(b.tryPlaceWord(paste)!=25)
-//			System.out.println("problem in placeWord for 3ed word (-10)");
-//
-//		Word mob=new Word(get("_OB"), 8, 7, false);
-//		if(b.tryPlaceWord(mob)!=18)
-//			System.out.println("problem in placeWord for 4th word (-10)");
-//
-//		Word bit=new Word(get("BIT"), 10, 4, false);
-//		if(b.tryPlaceWord(bit)!=22)
-//			System.out.println("problem in placeWord for 5th word (-15)");
-//
 
+		if( b.boardLegal(w0) || b.boardLegal(w1) || b.boardLegal(w2) ||b.boardLegal(w3)|| b.boardLegal(w4) || !b.boardLegal(w5) || !b.boardLegal(w6))
+			System.out.println("your boardLegal function is wrong (-10)");
+
+		for(Tile t : ts)
+			bag.put(t);
+
+		Word horn=new Word(get("HORN"), 7, 5, false);
+		if(b.tryPlaceWord(horn)!=14)
+			System.out.println("problem in placeWord for 1st word (-10)");
+
+		Word farm=new Word(get("FA_M"), 5, 7, true);
+		if(b.tryPlaceWord(farm)!=9)
+			System.out.println("problem in placeWord for 2ed word (-10)");
+
+		// creates the word farms
+		Word paste=new Word(get("PASTE"), 9, 5, false);
+		int score = b.tryPlaceWord(paste);
+		if(b.tryPlaceWord(paste)!=25)
+			System.out.println("problem in placeWord for 3ed word (-10)");
+
+		Word mob=new Word(get("_OB"), 8, 7, false);
+		if(b.tryPlaceWord(mob)!=18)
+			System.out.println("problem in placeWord for 4th word (-10)");
+
+		Word bit=new Word(get("BIT"), 10, 4, false);
+		if(b.tryPlaceWord(bit)!=22)
+			System.out.println("problem in placeWord for 5th word (-15)");
 	}
 
 	public static void main(String[] args) {
